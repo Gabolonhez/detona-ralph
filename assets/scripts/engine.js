4050d6ff77aca9d4d2ebea29e2195116b19271a0
Gabriel BF <gbbolonhez@gmail.com>
2024-11-28 00:55:02 -0300

    updating the game

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -6,8 +6,14 @@ const state = {
         score: document.querySelectorAll('#score'),
     },
     values: {
-        timerId: null,
-        gameVelocity: 1000
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
     }
 };
 
@@ -19,18 +25,34 @@ function randomSquare() {
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add('enemy');
+    state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity); 
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId); 
+        alert("Game Over! Your result is" + state.values.result);
+    }
 }
 
 function addListenerHitBox() {
-    state.view.squares.forEach((square) => {});
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++
+                state.view.score.textContent = state.values.results;
+                state.values.hitPosition = null;
+            }
+        }); 
+    });
 }
 
 function inicialize() {
-    randomSquare();
+    addListenerHitBox(); 
 }
 
 inicialize();
